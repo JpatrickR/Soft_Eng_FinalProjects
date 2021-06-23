d6300c6452e9161614b622bf63619fe5cd4e0641
Namocat <christiandhen@gmail.com>
2021-06-23 20:32:06 +0800

    added pay loan

--- main.c
+++ main.c
@@ -36,6 +36,59 @@ void view loan()
 }
 void pay loan() 
 {
+
+void pay_loan()
+{
+	prof in;
+	prof x[8];
+	char id[10];
+
+	int i, l, a;
+
+	printf("ID#: ");	scanf("%s",id);
+	sprintf(buf,"%s%s%s",path1,id,".txt");
+	p1 = fopen(buf,mode2);
+	if(p1 == NULL)
+	{
+		printf("Account not found");
+	}
+	else
+	{
+		fread(&in,sizeof(in),1,p1);
+			fread(&in,sizeof(in),1,p1);
+			if (feof(p1) !=0)
+			{
+				printf("ID#: %s\n", in.id);
+				printf("Student Name: %s\n", in.name);
+				printf("Degree: %s\n", in.degree);
+			}
+		printf("\nPay balance?");
+		printf("\n0. No\n1. Pay all balance");
+		printf("Choice: ");	scanf("%d",&a);
+
+		if (a == 0)
+		{
+			printf("You have selected <no>");
+		}
+			else if (a == 1)
+		{
+			printf("\nRe-enter ID#: ");	scanf("%d");
+			sprintf(buf,"%s%s%s",path3,id,".txt");
+			p3 = fopen(buf,mode1);
+			printf("Paying all balance...");
+				for (i = 0; i < 8; i++)
+				{
+					strcpy(x[i].remarks,"PAID\n");
+					fprintf(p3,"%s",x[i].remarks);
+				}
+			printf("Balance is fully paid...");
+			fclose(p3);
+		}
+	}
+	fclose(p1);
+
+}
+
 }
 void menu()
 {
