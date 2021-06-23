d7de553a8a0a94f34c9eb36b5fb0a18c34e337fc
JpatrickR <patrickramirez886@gmail.com>
2021-06-23 15:27:39 +0800

    Added viewloan code

--- main.c
+++ main.c
@@ -33,6 +33,74 @@ void start loan()
 }
 void view loan()
 {
+	prof in;
+	prof x[];
+	char id[5];
+
+	int i,l;
+
+	printf("Student ID#: ");	scanf("%s",id);
+	sprintf(buf,"%s%s%s",path1,id,".txt");
+
+	p1 = fopen(buf,mode2);
+	if (p1 == NULL)
+	{
+		printf("ERROR!...(😣)");
+	}
+		else
+	{
+		fread(&in,sizeof(in),1,p1);
+			if (feof(p1) !=0)
+			{
+		printf("ID#: %s\n", in.id);
+		printf("Student Name: %s\n", in.name);
+		printf("Degree: %s\n", in.degree);
+	}
+	}
+	fclose(p1);
+
+	printf("Re-enter ID#: ");	scanf("%s",id);
+	sprintf(buf,"%s%s%s",path2,id,".txt");
+
+	p2 = fopen(buf,mode2);
+	if (p2 == NULL)
+	{
+		printf("ERROR!");
+	}
+		else
+	{
+		fread(&in,sizeof(in),1,p2);
+		fscanf(p2,"%d",&in.loan);
+		fscanf(p2,"%d",&in.rate);
+		fscanf(p2,"%d",&in.week);
+		fscanf(p2,"%d",&in.gross);
+		fscanf(p2,"%d",&in.pay);
+
+		printf("\nLoan Amount: %d", in.loan);
+		printf("\nLoan Rate: %f", in.rate);
+		printf("\nWeeks: %f", in.week);
+		printf("\nTotal Amount: %.2f", in.gross);
+		printf("\nWeekly Payable Amount: %.2f", in.pay);
+		getch();
+
+	}
+
+	printf("\nRe-enter ID#: ");	scanf("%s",id);
+	sprintf(buf,"%s%s%s",path3,id,".txt");
+	p3 = fopen(buf,mode2);
+	l = 1;
+	printf("\n<<<Payment Status>>>");
+	printf("\n	WEEK	STATUS");
+		for(i = 0; i < 8; i++)
+		{
+			fscanf(p3,"%s",x[i].remarks);
+			printf(" \n	%d	%s",l+i,x.remarks);
+		}
+	fclose(p3);
+
+	fclose(p2);
+}
+
 }
 void pay loan() 
 {
