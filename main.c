fe5b3f9fe78c800af0c2c344908ca129b9129c63
Raid <rjalquizar@gmail.com>
2021-06-25 09:55:38 +0800

    final code for start loan with revisions in sprintf using path 2 instead of path 1

--- main.c
+++ main.c
@@ -42,7 +42,7 @@ void start loan()
     in.week = 8.0;
 
     printf("\nStudent ID#: "); scanf("%s",id);
-    sprintf(buf,"%s%s%s",path1,id,".txt");
+    sprintf(buf,"%s%s%s",path2,id2,".txt");
 
     p1 = fopen(buf,mode2);
     if (p1 == NULL)
@@ -56,6 +56,45 @@ void start loan()
 	    scanf("%s", id);	
 	    sprintf(buf,"%s%s%s",path2,id,".txt");
 	}
+	    p2 = fopen(buf,mode1);
+	    if (p2 == NULL)
+	    {
+		    printf("Error!...");
+	       }
+		else
+	       {
+		printf("Loan: ");   scanf("%d",&in.loan);
+		printf("\nInterest Rate: %f",in.rate);
+		printf("\nWeek Terms: %.2f", in.week);
+		    in.interest = in.loan * in.rate;
+		printf("\nInterest Amount: %.2f", in.interest);
+		    in.gross = in.loan + in.interest;
+		printf("\nTotal Gross: %.2f", in.gross);
+		    in.pay = in.gross / in.week;
+			printf("\nWeekly Payable Balance: %f",in.pay);
+		fwrite(&in,sizeof(in),1,p2);
+	    }
+	    fclose(p2);
+
+			printf("\nRe-enter ID#: ");	scanf("%s",id3);
+			sprintf(buf,"%s%s%s",path3,id3,".txt");
+
+			p3 = fopen(buf,mode1);
+			if (p3 == NULL)
+			{
+				printf("Error Making Chart!");
+			}
+				else
+			{
+				for(i = 0; i < 8; i++)
+				{
+					strcpy(x[i].remarks,"UNPAID\n");
+					fprintf(p3,"%s",x[i].remarks);
+				}
+			}
+			fclose(p3);
+    }
+    fclose(p1);
 
 }
 void view loan()
